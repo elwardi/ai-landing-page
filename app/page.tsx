aaac00576c19aa0e3a751683c4739b3bd05d0f09
elwardi <youssefxp07@gmail.com>
2025-09-22 14:17:51 +0100

    change some syntax eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { JSX, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
